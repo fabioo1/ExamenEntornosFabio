3acd910a24fd9fabacdc2f69d14d936da6139274
Fabio <informatica@escuelaces.com>
2022-06-01 12:12:08 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -13,7 +13,14 @@ package Cheese;
 public class Main {
     
      public static void main(String[] args) {
-    
+         
          System.out.println("1º Gdam: Entornos de desarrollo");
+         String cod1 = "001";
+         Producto prod1 = new Producto(cod1, "semidesnatada", 2, 1.2);
+         System.out.println(prod1);
+         
+         String cod2 = "002";        
+         Producto prod2 = new Producto(cod2, "entera", 3, 1.2);
+         System.out.println(prod2);
     }
 }
